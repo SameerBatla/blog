74a4b8b359eef2a4ae14966d6318142018e3b28d
SameerBatla <sambatla456@gmail.com>
2021-02-05 00:40:50 +0530

    err handle

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
+  const [loading, setloading] = useState(true);
+  const [error, seterror] = useState(null);
+
+  const getData = async () => {
+    try {
+      const res = await fetch('http://localhost:8000/blogs');
+      if (!res.ok) {
+        throw new Error('something went Wrong');
+      }
+      const data = await res.json();
+      setBlogs(data);
+      setloading(false);
+      seterror(null);
+    } catch (err) {
+      if (err.name === 'AbortError') console.log('fetchaborted');
+      else {
+        setloading(false);
+        seterror(err.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <React.Fragment>
-      {blogs.length > 0 && (
+      {loading && <div>Loading......</div>}
+      {error && <div>{error}</div>}
+      {blogs && (
         <div className="home">
           <BlogList blogs={blogs} title="All Blogs" />
+          <BlogList
+            blogs={blogs.filter((blog) => blog.author === 'mario')}
+            title="Mario Blogs"
+          />
         </div>
       )}
     </React.Fragment>
